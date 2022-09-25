68c598e087ba1563251c14645aff39189e9b371f
Puzakov Maxym <m.puzakov90@gmail.com>
2022-09-25 20:17:41 +0300

    new files

---
+++ js/main.js
@@ -0,0 +1,47 @@
+/*
+let text = 'Сегодня пион целый день, ромашка, звучат, пион тревоги ромашка';
+
+console.log(text);
+
+while (text.includes('ромашка', 'пион')) {
+    text = text.replace ('ромашка', '*******');
+    text = text.replace ('пион', '****');
+}
+
+console.log(text);
+*/
+
+
+let text = 'Сегодня пион целый день, ромашка, звучат, пион тревоги ромашка';
+console.log(text);
+
+let newText = 'Сегодня пион целый день, ромашка, звучат, пион тревоги ромашка';
+console.log(badWordFilter(newText));
+
+function badWordFilter() {
+    while (newText.includes ('ромашка', 'пион')) {
+        newText = newText.replace('ромашка', '*******');
+        newText = newText.replace('пион', '****');
+    }
+    
+    return newText;
+}
+
+
+/*
+let text = 'Сегодня пион целый день, ромашка, звучат, пион тревоги ромашка';
+console.log(text);
+
+let newText = 'Сегодня пион целый день, ромашка, звучат, пион тревоги ромашка';
+console.log(badWordFilter(newText));
+
+function badWordFilter(badWord1, badWord2) {
+    if (badWord1.includes('ромашка')) {
+        newText = newText.replace('ромашка', '*******');
+    } else if (badWord2.includes('пион')) {
+        newText = newText.replace('пион', '****');
+    }
+
+    return newText;
+}
+*/
